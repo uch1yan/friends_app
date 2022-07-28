0c8ff5653b2bbafd74a77d34804afaf098b4b02e
manami young <manayou1125@outlook.jp>
2022-07-28 22:20:47 +0900

    Pictureモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   has_secure_password
   validates :password, length: { minimum: 6 }
+  has_many :pictures
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_092633) do
+ActiveRecord::Schema.define(version: 2022_07_28_131439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_07_28_092633) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pictures", "users"
 end
