18e68cbc3e1e27ad349cbec83d0e579361d31d0e
manami young <manayou1125@outlook.jp>
2022-07-28 21:12:39 +0900

    sessionのnew.html.erbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
